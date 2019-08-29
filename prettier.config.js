e317fde6db45f9561e8fc88a5ac8cb3a919762d4
HansHammel <mail@specialsolutions.de>
2019-08-29 14:46:47 +0200

    more changes

--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  trailingComma: "es5",
-  //tabWidth: 4,
+  trailingComma: 'es5',
+  // tabWidth: 4,
   semi: true,
   singleQuote: true,
-  arrowParens: "always",
-  editorconfig: true
+  arrowParens: 'always',
+  editorconfig: true,
 };
--- test/setup.js
+++ test/setup.js
@@ -1,48 +1,50 @@
-import { EventEmitter } from 'events'
-import MongodbMemoryServer from 'mongodb-memory-server'
-import mongoose from '../src/services/mongoose'
+import { EventEmitter } from 'events';
+import MongodbMemoryServer from 'mongodb-memory-server';
+import mongoose from '../src/services/mongoose';
 
-EventEmitter.defaultMaxListeners = Infinity
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000
+EventEmitter.defaultMaxListeners = Infinity;
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
 
-global.Array = Array
-global.Date = Date
-global.Function = Function
-global.Math = Math
-global.Number = Number
-global.Object = Object
-global.RegExp = RegExp
-global.String = String
-global.Uint8Array = Uint8Array
-global.WeakMap = WeakMap
-global.Set = Set
-global.Error = Error
-global.TypeError = TypeError
-global.parseInt = parseInt
-global.parseFloat = parseFloat
+global.Array = Array;
+global.Date = Date;
+global.Function = Function;
+global.Math = Math;
+global.Number = Number;
+global.Object = Object;
+global.RegExp = RegExp;
+global.String = String;
+global.Uint8Array = Uint8Array;
+global.WeakMap = WeakMap;
+global.Set = Set;
+global.Error = Error;
+global.TypeError = TypeError;
+global.parseInt = parseInt;
+global.parseFloat = parseFloat;
 
-let mongoServer
-
-
+let mongoServer;
 
 beforeAll(async () => {
-  mongoServer = new MongodbMemoryServer()
-  const mongoUri = await mongoServer.getConnectionString()
-  await mongoose.connect(mongoUri, { useNewUrlParser: true, useCreateIndex: true }, (err) => {
-    if (err) console.error(err)
-  })
-}, 60000) // May require additional time for downloading MongoDB binaries
+  mongoServer = new MongodbMemoryServer();
+  const mongoUri = await mongoServer.getConnectionString();
+  await mongoose.connect(
+    mongoUri,
+    { useNewUrlParser: true, useCreateIndex: true },
+    (err) => {
+      if (err) console.error(err);
+    }
+  );
+}, 60000); // May require additional time for downloading MongoDB binaries
 
 afterAll(async () => {
-  await mongoose.disconnect()
-  await mongoServer.stop()
-})
+  await mongoose.disconnect();
+  await mongoServer.stop();
+});
 
 afterEach(async () => {
-  const { collections } = mongoose.connection
-  const promises = []
+  const { collections } = mongoose.connection;
+  const promises = [];
   Object.keys(collections).forEach((collection) => {
-    promises.push(collections[collection].deleteMany())
-  })
-  await Promise.all(promises)
-})
+    promises.push(collections[collection].deleteMany());
+  });
+  await Promise.all(promises);
+});
